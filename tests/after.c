3178682a71431329e95839e70fbf67bf47576bcc
binoll <onionlsfa@gmail.com>
2024-07-27 16:19:02 +0300

    docs: upated README.md

---
+++ tests/after.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>                       
+  
+                       
+
+int main() {     
+  
+                                    
+      
+   char *str = "string example"; 
+   char  buf[32];                        
+   buf[0] = '\0';                        
+   printf("string: \"%s\"//\n", str);   
+        
+   printf("buffer before copying: \"%s\"//\n", buf)		; 
+   strcpy(buf, str); 
+   printf("buffer// after copying: \"//%s\"\n", buf)		;         
+ 
+return 0;
+}
